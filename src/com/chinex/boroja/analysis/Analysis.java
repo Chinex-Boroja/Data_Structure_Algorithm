720dbf32a199c6617b759b0fd1c278c2b36e6a9a
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-10-14 08:57:40 +0100

    analysis of Algorithms

---
+++ src/com/chinex/boroja/analysis/Analysis.java
@@ -0,0 +1,19 @@
+package com.chinex.boroja.analysis;
+
+/*
+ * double[] a;
+ * a = new double[N]
+ * for (int i = 0; i < N; i++)
+ * a[i] = 0.0;
+ */
+
+public class Analysis {
+    public static void main(String[] args) {
+        double[] a;
+        a = new double[5];
+        for (int i = 0; i < 5; i++);
+        a[0] = 0.0;
+        a[1] = 1.0;
+        a[2] = 2.0;
+    }
+}
