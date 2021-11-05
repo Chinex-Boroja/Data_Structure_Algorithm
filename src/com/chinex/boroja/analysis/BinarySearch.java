84f8e55599d5a978d02275330698b77a3ed2dd59
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-11-05 15:27:50 +0100

    add Binary search algo

--- src/com/chinex/boroja/analysis/BinarySearch.java
+++ src/com/chinex/boroja/analysis/BinarySearch.java
@@ -18,3 +18,16 @@ public class BinarySearch {
         else return mid;
     }
 }
+class BinarySearchAlgo {
+    public static int binarySearch(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
