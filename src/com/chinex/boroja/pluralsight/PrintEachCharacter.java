efb45afe835c6f1f4f863cc442158c00e0a07679
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-06-22 12:51:50 +0100

    Print each character

---
+++ src/com/chinex/boroja/pluralsight/PrintEachCharacter.java
@@ -0,0 +1,11 @@
+package com.chinex.boroja.pluralsight;
+
+public class PrintEachCharacter {
+    public static void main(String[] args) {
+        char[] letters = {'a', 'b', 'c', 'd', 'e'};
+
+        for (char letter : letters) {
+            System.out.println(letter);
+        }
+    }
+}
