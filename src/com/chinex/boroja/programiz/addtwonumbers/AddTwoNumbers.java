608dbe2c676e11bce91e80e070817cb8c215f529
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-12-20 11:50:32 +0100

    Chore: Algo for largest of three numbers

--- src/com/chinex/boroja/programiz/addtwonumbers/AddTwoNumbers.java
+++ src/com/chinex/boroja/programiz/addtwonumbers/AddTwoNumbers.java
@@ -4,9 +4,21 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
         System.out.println(addTwoNumbers(8, 9));
 
+        System.out.println("The largest of the three numbers is: " + findLargestOfThreeNumbers(11, 10, 16));
     }
     public static int addTwoNumbers(int num1, int num2) {
         return num1 + num2;
+    }
+    public static int findLargestOfThreeNumbers(int num1, int num2, int num3) {
+
+        if (num1 > num2 && num1 > num3) {
+            return num1;
+        } else if (num2 > num1 && num2 > num3) {
+            return num2;
+        }
+        else {
+            return num3;
+        }
     }
 }
 
