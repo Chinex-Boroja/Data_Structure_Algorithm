4735ce9e1df02f17d1aff0faebfbcc0c58c93d6c
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-12-22 09:07:55 +0100

    Checking time complexities of two algos

--- src/com/chinex/boroja/programiz/addtwonumbers/AddTwoNumbers.java
+++ src/com/chinex/boroja/programiz/addtwonumbers/AddTwoNumbers.java
@@ -2,14 +2,21 @@ package com.chinex.boroja.programiz.addtwonumbers;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        System.out.println(addTwoNumbers(8, 9));
+        double now = System.currentTimeMillis();
 
-        System.out.println("The largest of the three numbers is: " + findLargestOfThreeNumbers(11, 10, 16));
+        AddTwoNumbers demo = new AddTwoNumbers();
+        System.out.println(demo.findSumOfNaturalNum(99999));
+        System.out.println("Time taken - " + (System.currentTimeMillis() - now) + " millisecs.");
+
+        System.out.println(demo.addTwoNumbers(8, 9));
+
+        System.out.println("The largest of the three numbers is: " + demo.findLargestOfThreeNumbers(11, 10, 16));
+        System.out.println("The sum of natural occurring numbers is: " + demo.findSumOfNaturalNum(7));
     }
-    public static int addTwoNumbers(int num1, int num2) {
+    public int addTwoNumbers(int num1, int num2) {
         return num1 + num2;
     }
-    public static int findLargestOfThreeNumbers(int num1, int num2, int num3) {
+    public int findLargestOfThreeNumbers(int num1, int num2, int num3) {
 
         if (num1 > num2 && num1 > num3) {
             return num1;
@@ -19,6 +26,18 @@ public class AddTwoNumbers {
         else {
             return num3;
         }
+    }
+    public int findSumOfNaturalNumbers(int num) {
+        return (num * (num + 1)) / 2;
+    }
+
+    public int findSumOfNaturalNum(int number) {
+        int sum = 0;
+
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
 
