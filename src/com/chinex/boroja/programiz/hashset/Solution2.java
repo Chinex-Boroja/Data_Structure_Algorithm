fc77e75ffce4e9e8020d12ef53cb5c9a9ce9aa66
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-11-13 17:05:48 +0100

    add solution two

--- src/com/chinex/boroja/programiz/hashset/Solution2.java
+++ src/com/chinex/boroja/programiz/hashset/Solution2.java
@@ -1,2 +1,32 @@
-package com.chinex.boroja.programiz.hashset;public class Solution2 {
+package com.chinex.boroja.programiz.hashset;
+
+import java.util.HashSet;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(digitCount("1210"));
+    }
+    public static boolean digitCount(String num) {
+        HashSet<Character> uniqueDigits = new HashSet<>();
+        for (char ch: num.toCharArray()) {
+            if (ch < '0' || ch > '9') {
+                return false; // Check if the character is a valid digit
+            }
+            uniqueDigits.add(ch);
+        }
+
+        for (char ch: uniqueDigits) {
+            int count = 0;
+            for (char digitChar :num.toCharArray()) {
+                if (digitChar == ch) {
+                    count++;
+                }
+            }
+            if (count != Character.getNumericValue(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
