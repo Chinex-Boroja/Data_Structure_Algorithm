6a9419872259f0eb706d545b1a772e78f3447be1
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-11-13 17:04:58 +0100

    add largest num

--- src/com/chinex/boroja/programiz/largest/LargestNumber.java
+++ src/com/chinex/boroja/programiz/largest/LargestNumber.java
@@ -1,2 +1,27 @@
-package com.chinex.boroja.programiz.largest;public class LargestNumber {
+package com.chinex.boroja.programiz.largest;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int largest = findLargest(11, 10, 12);
+
+        System.out.println("The largest of the three numbers is: " + largest);
+    }
+
+    public static int findLargest(int num1, int num2, int num3) {
+
+        if (num1 > num2) {
+            if (num1 > num3) {
+                return num1;
+            }
+            else {
+                return num3;
+            }
+        }
+        else
+            if (num2 > num3 ) {
+                return num2;
+            }
+            else
+                return num3;
+    }
 }
